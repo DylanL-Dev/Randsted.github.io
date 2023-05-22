869dc0ee31accbfbebbf5f69837ec2c4b7b32ee8
Dylan LEFEBVRE <dylan.lefebvre76@live.com>
2023-05-22 15:58:18 +0200

    Modification site V.19

--- script.js
+++ script.js
@@ -7,3 +7,17 @@ function closeNav() {
   document.getElementById("myNav").style.width = "0%";
 }
 // End Nav barre annimation
+
+// lanceur video au clik
+
+const videos = document.querySelectorAll('.myVideo');
+
+videos.forEach(function(video) {
+  video.addEventListener('click', function() {
+    if (video.paused) {
+      video.play();
+    } else {
+      video.pause();
+    }
+  });
+});
